f3a93f05bb340724753ae82917978dc23b950014
sabin-vv <vvsabin@gmail.com>
2026-04-05 13:52:54 +0530

    fix: add icon support in InputField component

--- frontend/src/shared/components/InputField/InputField.tsx
+++ frontend/src/shared/components/InputField/InputField.tsx
@@ -4,7 +4,9 @@ import styles from './InputField.module.css'
 import type { InputFieldProps } from './InputField.types'
 
 const InputField = ({ label, errors, icon, prefix, ...props }: InputFieldProps) => {
-    const inputClass = `${styles.input} ${prefix ? styles.withPrefix : ''}`
+    const hasIcon = !!icon
+    const hasPrefix = !!prefix
+    const inputClass = `${styles.input} ${hasIcon ? styles.withLeft : ''} ${hasPrefix ? styles.withPrefix : ''}`
     return (
         <div className={styles.formFields}>
             <label htmlFor={props.id}>{label}</label>
