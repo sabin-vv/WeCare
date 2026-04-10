233b9318b10096c117d4b7d281fab975baef0ae9
sabin-vv <vvsabin@gmail.com>
2026-04-10 10:46:19 +0530

    fix: update the doctor route name properly

--- frontend/src/modules/doctor/api/doctor.api.ts
+++ frontend/src/modules/doctor/api/doctor.api.ts
@@ -2,7 +2,7 @@ import type { ApiInterface } from '@/modules/auth/api/auth.api.types'
 import { api } from '@/services/api'
 
 export const updateProfile = async (data: FormData): Promise<ApiInterface> => {
-    const res = await api.post('/doctor/profile', data)
+    const res = await api.post('/doctors/profile', data)
 
     return res.data
 }
