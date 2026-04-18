61d40cbe548978943d5aab0a631384c3d4e8753a
sabin-vv <vvsabin@gmail.com>
2026-04-18 11:52:48 +0530

    feat: add createProfile, getProfile, updateProfile to caregiver service interface

--- backend/src/modules/caregiver/interfaces/caregiver.service.interface.ts
+++ backend/src/modules/caregiver/interfaces/caregiver.service.interface.ts
@@ -1,7 +1,11 @@
 import { MulterFiles } from '../../auth/types/auth.types'
-import { CaregiverDocument } from '../types/caregiver.types'
-import { RegisterCaregiverDTO } from '../validator/caregiver.schema'
+import { CaregiverDocument, CaregiverProfileResponse } from '../types/caregiver.types'
+import { CreateCaregiverProfileDTO, RegisterCaregiverDTO } from '../validator/caregiver.schema'
+import { UpdateCaregiverSettingsDTO } from '../validator/updateCaregiverSettings.schema'
 
 export interface ICaregiverService {
     registerCaregiver(dto: RegisterCaregiverDTO, files: MulterFiles): Promise<CaregiverDocument>
+    createProfile(userId: string, dto: CreateCaregiverProfileDTO): Promise<CaregiverDocument>
+    getProfile(userId: string): Promise<CaregiverProfileResponse>
+    updateProfile(userId: string, dto: UpdateCaregiverSettingsDTO): Promise<CaregiverProfileResponse>
 }
