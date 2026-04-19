c236ca1d5611159df1467c178e3c39cfed0fc220
sabin-vv <vvsabin@gmail.com>
2026-04-19 23:02:47 +0530

    feat(doctor): create DayScheduleRow component

---
+++ frontend/src/modules/doctor/components/DayScheduleRow.tsx
@@ -0,0 +1,48 @@
+import type { DayScheduleRowProps } from '../types/doctor.types'
+
+import styles from './DayScheduleRow.module.css'
+import TimeRangeInput from './TimeRangeInput'
+
+import ToggleSwitch from '@/shared/components/ToggleSwitch/ToggleSwitch'
+
+export const DayScheduleRow = ({
+    data,
+    slotDuration,
+    canAddRange,
+    onToggleAvailability,
+    onRangeChange,
+    onAddRange,
+    onDeleteRange,
+}: DayScheduleRowProps) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <ToggleSwitch checked={data.isAvailable} onChange={onToggleAvailability} />
+                <span className={styles.day}>{data.day}</span>
+            </div>
+
+            <div className={styles.right}>
+                {data.isAvailable ? (
+                    <>
+                        {data.timeRanges.map((range, i) => (
+                            <div key={i} className={styles.rangeRow}>
+                                <TimeRangeInput
+                                    value={range}
+                                    slotDuration={slotDuration}
+                                    onChange={(value) => onRangeChange(i, value)}
+                                    onDelete={() => onDeleteRange(i)}
+                                />
+                            </div>
+                        ))}
+
+                        <button type="button" className={styles.addBtn} onClick={onAddRange} disabled={!canAddRange}>
+                            +
+                        </button>
+                    </>
+                ) : (
+                    <span className={styles.inactive}>Not active</span>
+                )}
+            </div>
+        </div>
+    )
+}
