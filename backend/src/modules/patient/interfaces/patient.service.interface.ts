21f74fd8df4d6a6f11618c4513eba232298b8307
sabin-vv <vvsabin@gmail.com>
2026-04-27 20:04:55 +0530

    feat: add getProfile and updateProfile methods to patient service interface

--- backend/src/modules/patient/interfaces/patient.service.interface.ts
+++ backend/src/modules/patient/interfaces/patient.service.interface.ts
@@ -1,6 +1,9 @@
-import { PatientResponseDTO } from '../mapper/patient.mapper'
+import { PatientProfileResponseDTO, PatientResponseDTO } from '../mapper/patient.mapper'
 import { RegisterPatientDTO } from '../validator/patient.schema'
+import { UpdatePatientSettingsDTO } from '../validator/updatePatientSettings.schema'
 
 export interface IPatientService {
     registerPatient(dto: RegisterPatientDTO): Promise<PatientResponseDTO>
+    getProfile(userId: string): Promise<PatientProfileResponseDTO>
+    updateProfile(userId: string, dto: UpdatePatientSettingsDTO): Promise<PatientProfileResponseDTO>
 }
