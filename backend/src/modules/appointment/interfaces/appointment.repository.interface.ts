4aeeced0c7b6b89f99b264a34391211e0d588895
sabin-vv <vvsabin@gmail.com>
2026-04-28 23:32:32 +0530

    feat: add IAppointmentRepository interface

---
+++ backend/src/modules/appointment/interfaces/appointment.repository.interface.ts
@@ -0,0 +1,17 @@
+import { AppointmentDocument } from '../types/appointment.types'
+
+export interface IAppointmentRepository {
+    create(data: Partial<AppointmentDocument>): Promise<AppointmentDocument>
+
+    findById(id: string): Promise<AppointmentDocument | null>
+
+    update(id: string, data: Partial<AppointmentDocument>): Promise<AppointmentDocument | null>
+
+    findByPatientId(patientId: string): Promise<AppointmentDocument[]>
+
+    findByDoctorId(doctorId: string): Promise<AppointmentDocument[]>
+
+    findActiveAppointments(doctorId: string, date: string): Promise<AppointmentDocument[]>
+
+    findActiveByPatientAndDoctor(patientId: string, doctorId: string): Promise<AppointmentDocument | null>
+}
