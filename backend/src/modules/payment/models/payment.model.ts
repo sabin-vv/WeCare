dcf0a4333ebae865be740c80abf99b5a3fd1d8f4
sabin-vv <vvsabin@gmail.com>
2026-04-28 23:50:48 +0530

    feat: add payment model with optional razorpayOrderId and paidAt

---
+++ backend/src/modules/payment/models/payment.model.ts
@@ -0,0 +1,61 @@
+import { model, Schema, Types } from 'mongoose'
+
+import { PaymentDocument } from '../types/payment.types'
+
+const paymentSchema = new Schema<PaymentDocument>(
+    {
+        patientId: {
+            type: Types.ObjectId,
+            ref: 'Patient',
+            required: true,
+        },
+        caregiverId: {
+            type: Types.ObjectId,
+            ref: 'Caregiver',
+        },
+        appointmentId: {
+            type: Types.ObjectId,
+            ref: 'Appointment',
+        },
+        // subscriptionId: {
+        //     types: Types.ObjectId,
+        //     ref: 'Subscription',
+        // },
+        paymentType: {
+            type: String,
+            enum: ['consultation', 'subscription'],
+            required: true,
+        },
+        consultationFee: {
+            type: Number,
+        },
+        platformFee: {
+            type: Number,
+        },
+        totalAmount: {
+            type: Number,
+            required: true,
+        },
+        razorpayOrderId: {
+            type: String,
+        },
+        razorpayPaymentId: {
+            type: String,
+        },
+        razorpaySignature: {
+            type: String,
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'success', 'failed', 'refunded'],
+            default: 'pending',
+            required: true,
+        },
+        paidAt: {
+            type: Date,
+        },
+    },
+    { timestamps: true },
+)
+
+export const PaymentModel = model<PaymentDocument>('Payment', paymentSchema)
