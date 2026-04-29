e736a18087b6ba7e79d06c9ce4d6405c7556085d
sabin-vv <vvsabin@gmail.com>
2026-04-29 20:11:00 +0530

    fix: removed register service interface

--- backend/src/modules/caregiver/interfaces/caregiver.service.interface.ts
+++ backend/src/modules/caregiver/interfaces/caregiver.service.interface.ts
@@ -1,11 +1,9 @@
-import { MulterFiles } from '../../auth/types/auth.types'
 import { CaregiverProfileResponse } from '../types/caregiver.types'
-import { CreateCaregiverProfileDTO, RegisterCaregiverDTO } from '../validator/caregiver.schema'
+import { CreateCaregiverProfileDTO } from '../validator/caregiver.schema'
 import { UpdateCaregiverSettingsDTO } from '../validator/updateCaregiverSettings.schema'
 
 export interface ICaregiverService {
-    registerCaregiver(dto: RegisterCaregiverDTO, files: MulterFiles): Promise<CaregiverProfileResponse>
-    createProfile(userId: string, dto: CreateCaregiverProfileDTO): Promise<CaregiverProfileResponse>
+    createProfile(userId: string, dto: CreateCaregiverProfileDTO): Promise<Partial<CaregiverProfileResponse>>
     getProfile(userId: string): Promise<CaregiverProfileResponse>
     updateProfile(userId: string, dto: UpdateCaregiverSettingsDTO): Promise<CaregiverProfileResponse>
 }
