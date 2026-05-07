773f4c289addd58857788a4ae34a12393d350d40
sabin-vv <vvsabin@gmail.com>
2026-05-07 17:06:07 +0530

    feat: inintialize cron job in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,14 @@ import './container'
 import app from './app'
 import { connectDB } from './core/config/db'
 import { env } from './core/config/env'
+import { initializeCrons } from './core/cron'
 import { logger } from './core/logger/logger'
 
 const PORT = env.PORT
 
 const startServer = async () => {
     await connectDB()
+    initializeCrons()
 
     app.listen(PORT, () => {
         logger.info(`Server running at port ${PORT}`)
