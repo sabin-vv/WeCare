0435d7e062784a993c734551d673cb09a95871f6
sabin-vv <vvsabin@gmail.com>
2026-05-11 18:30:06 +0530

    refactor: refactor the caregiver module apis with constant

--- frontend/src/modules/caregiver/api/caregiver.api.ts
+++ frontend/src/modules/caregiver/api/caregiver.api.ts
@@ -2,18 +2,19 @@ import type { CaregiverProfileResponse } from '../types/caregiver.types'
 
 import type { ApiInterface } from '@/modules/auth/api/auth.api.types'
 import { api } from '@/services/api'
+import { CAREGIVERS_API } from '@/shared/constants/api.constants'
 
 export const createCaregiverProfile = async (formData: FormData): Promise<ApiInterface> => {
-    const res = await api.post('/caregivers/profile', formData)
+    const res = await api.post(`${CAREGIVERS_API}/profile`, formData)
     return res.data
 }
 
 export const getCaregiverProfile = async (): Promise<CaregiverProfileResponse> => {
-    const res = await api.get<CaregiverProfileResponse>('/caregivers/me')
+    const res = await api.get<CaregiverProfileResponse>(`${CAREGIVERS_API}/me`)
     return res.data
 }
 
 export const updateCaregiverProfile = async (data: Record<string, unknown>): Promise<CaregiverProfileResponse> => {
-    const res = await api.put<CaregiverProfileResponse>('/caregivers/me', data)
+    const res = await api.put<CaregiverProfileResponse>(`${CAREGIVERS_API}/me`, data)
     return res.data
 }
