457d1d9890aa4c1f6bb2782753d4b85c458e4293
sabin-vv <vvsabin@gmail.com>
2026-05-11 18:08:22 +0530

    feat: created the api constant file

---
+++ frontend/src/shared/constants/api.constants.ts
@@ -0,0 +1,21 @@
+export const AUTH_API = '/auth'
+
+export const DOCTORS_API = '/doctors'
+
+export const PATIENTS_API = '/patients'
+
+export const CAREGIVER_API = '/caregivers'
+
+export const UPLOADS_API = '/uploads'
+
+export const ADMIN_API = '/admin'
+
+export const PAYMENTS_API = '/payments'
+
+export const WALLET_API = '/wallet'
+
+export const APPOINTMENT_API = '/appointments'
+
+export const PRESCRIPTIONS_API = '/prescriptions'
+
+export const VITALS_API = '/vitals'
