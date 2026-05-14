a26610eca18894898272980e3eb77dbc57c9b516
sabin-vv <vvsabin@gmail.com>
2026-05-14 18:44:46 +0530

    feat(subscription): add findById to ISubscriptionRepository interface

--- backend/src/modules/subscription/interfaces/subscription.repository.interface.ts
+++ backend/src/modules/subscription/interfaces/subscription.repository.interface.ts
@@ -3,6 +3,9 @@ import type { Types } from 'mongoose'
 import type { SubscriptionDocument } from '../types/subscription.types'
 
 export interface ISubscriptionRepository {
+    findById(id: string): Promise<SubscriptionDocument | null>
     findByPatientId(patientId: Types.ObjectId): Promise<SubscriptionDocument | null>
     findActiveByPatient(patientId: Types.ObjectId): Promise<SubscriptionDocument | null>
+    create(data: Partial<SubscriptionDocument>): Promise<SubscriptionDocument>
+    updateById(id: string, data: Partial<SubscriptionDocument>): Promise<SubscriptionDocument | null>
 }
