20b6c1aaa90039d1de0cd69938cfa423cdb164f5
sabin-vv <vvsabin@gmail.com>
2026-05-16 13:30:57 +0530

    refactor: use shared navLinks in PatientHeader

--- frontend/src/modules/patient/component/PatientHeader.tsx
+++ frontend/src/modules/patient/component/PatientHeader.tsx
@@ -1,12 +1,7 @@
 import Header from '@/shared/components/Header/Header'
+import { patientNavLinks } from '@/shared/constants/navLinks'
 
-const caregiverNavLinks = [
-    { label: 'Dashboard', path: '/dashboard' },
-    { label: 'Find Dotors', path: '/doctors' },
-    { label: 'Appointments', path: '/appointments' },
-    { label: 'Wallet', path: '/wallet' },
-]
 const PatientHeader = () => {
-    return <Header navLinks={caregiverNavLinks} />
+    return <Header navLinks={patientNavLinks} />
 }
 export default PatientHeader
