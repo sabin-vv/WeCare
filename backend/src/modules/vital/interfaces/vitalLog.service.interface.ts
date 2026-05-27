f990f48bb421224bbfe77556ba5b889bbc785ef6
sabin-vv <vvsabin@gmail.com>
2026-05-27 22:42:18 +0530

    cleanup(vital): remove leftover vitalLog service interface

--- backend/src/modules/vital/interfaces/vitalLog.service.interface.ts
+++
@@ -1,7 +0,0 @@
-import { VitalLogDocument } from '../types/vitalLog.types'
-import { CreateVitalLogDTO } from '../validator/vitalLog.schema'
-
-export interface IVitalLogService {
-    create(userId: string, data: CreateVitalLogDTO): Promise<VitalLogDocument>
-    getPatientLogs(patientId: string): Promise<VitalLogDocument[]>
-}
