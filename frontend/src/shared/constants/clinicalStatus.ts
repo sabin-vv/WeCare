f355b0a0f4a570f762808e9d83f17d5a4cb38040
sabin-vv <vvsabin@gmail.com>
2026-05-29 13:21:08 +0530

    fix: update the clinical transition of hospitalized state

--- frontend/src/shared/constants/clinicalStatus.ts
+++ frontend/src/shared/constants/clinicalStatus.ts
@@ -2,7 +2,7 @@ import type { ClinicalStatus } from '@/modules/doctor/types/doctor.types'
 
 export const CLINICAL_STATUS_TRANSITION: Record<ClinicalStatus, ClinicalStatus[]> = {
     active: ['hospitalized', 'recovered', 'deceased'],
-    hospitalized: ['active', 'recovered'],
+    hospitalized: ['active', 'recovered','deceased'],
     recovered: ['active', 'hospitalized'],
     deceased: [],
 } as const
