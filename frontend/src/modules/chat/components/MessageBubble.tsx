419f5cc850e5e96b5f7d1b414d0e941444bd4120
sabin-vv <vvsabin@gmail.com>
2026-07-03 15:50:38 +0530

    feat: add MessageBubble component

---
+++ frontend/src/modules/chat/components/MessageBubble.tsx
@@ -0,0 +1,24 @@
+import styles from './Chat.module.css'
+
+import type { MessageBubbleProps } from '@/modules/chat/types/chat.types'
+
+const formatTime = (dateStr: string) => {
+    const d = new Date(dateStr)
+    return d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+}
+
+const MessageBubble = ({ message, isOwn }: MessageBubbleProps) => {
+    return (
+        <div className={`${styles.bubbleRow} ${isOwn ? styles.bubbleRowEnd : styles.bubbleRowStart}`}>
+            <div className={`${styles.bubble} ${isOwn ? styles.bubbleOwn : styles.bubbleOther}`}>
+                <div className={styles.bubbleText}>{message.message}</div>
+
+                <div className={styles.footer}>
+                    <span className={styles.bubbleTime}>{formatTime(message.createdAt)}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageBubble
