903517f8c2db29747de310e9d635d4a366c5f7ed
sabin-vv <vvsabin@gmail.com>
2026-07-06 11:52:17 +0530

    refactor: add shared PaginationData type and DEFAULT_PAGINATION constant

---
+++ frontend/src/shared/constants/pagination.constants.ts
@@ -0,0 +1,8 @@
+import type { PaginationData } from '../types/pagination.types'
+
+export const DEFAULT_PAGINATION: PaginationData = {
+    page: 1,
+    limit: 8,
+    totalCount: 0,
+    totalPages: 1,
+}
---
+++ frontend/src/shared/types/pagination.types.ts
@@ -0,0 +1,6 @@
+export interface PaginationData {
+    page: number
+    limit: number
+    totalCount: number
+    totalPages: number
+}
