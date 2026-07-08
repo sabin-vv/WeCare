387658bf83fc457589495846ffef95befec2438b
sabin-vv <vvsabin@gmail.com>
2026-07-08 22:35:53 +0530

    Fix NavLink type import

--- frontend/src/layout/PublicLayout.tsx
+++ frontend/src/layout/PublicLayout.tsx
@@ -5,13 +5,13 @@ import styles from './PublicLayout.module.css'
 import { Role } from '@/modules/auth/types/auth.types'
 import Footer from '@/shared/components/Footer/Footer'
 import Header from '@/shared/components/Header/Header'
-import type { NavLink } from '@/shared/components/Header/Header.types'
+import type { NavLinks } from '@/shared/components/Header/Header.types'
 import { caregiverNavLinks, doctorNavLinks, patientNavLinks } from '@/shared/constants/navLinks'
 import { useAuth } from '@/shared/context/AuthContext'
 
 const PublicLayout = () => {
     const { user } = useAuth()
-    let navLinks: NavLink[] = []
+    let navLinks: NavLinks[] = []
 
     if (user) {
         switch (user.role) {
