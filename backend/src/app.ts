bc216539df1e137c0a9231efaa2191ba8e31065e
sabin-vv <vvsabin@gmail.com>
2026-07-09 11:30:03 +0530

    Add production domain to CORS origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,7 +33,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://wecarehealth.online'],
         credentials: true,
     }),
 )
