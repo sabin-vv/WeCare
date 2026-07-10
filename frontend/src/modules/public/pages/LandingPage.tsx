bda14a0104392188b4858de4157c8cdc3aac65ac
sabin-vv <vvsabin@gmail.com>
2026-07-10 23:33:25 +0530

    test frontend deploy

--- frontend/src/modules/public/pages/LandingPage.tsx
+++ frontend/src/modules/public/pages/LandingPage.tsx
@@ -9,6 +9,7 @@ const LandingPage = () => {
 
     return (
         <div className={styles.landingContainer}>
+            <h1>TeSt Deploy</h1>
             <section className={styles.heroSection}>
                 <div className={styles.heroInner}>
                     <h1 className={styles.heroTitle}>
