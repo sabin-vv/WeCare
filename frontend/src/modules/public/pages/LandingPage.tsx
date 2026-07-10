fee68c3ba84e22b3a43f515db457cfb9ae17e828
sabin-vv <vvsabin@gmail.com>
2026-07-10 18:04:26 +0530

    Test CI/CD:2

--- frontend/src/modules/public/pages/LandingPage.tsx
+++ frontend/src/modules/public/pages/LandingPage.tsx
@@ -7,7 +7,7 @@ import Button from '@/shared/components/Button/Button'
 const LandingPage = () => {
     const navigate = useNavigate()
 
-    console.log('CI CD test')
+    console.log('CI CD test: 2')
     return (
         <div className={styles.landingContainer}>
             <section className={styles.heroSection}>
